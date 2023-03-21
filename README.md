b98387155eb35fe4e6a46d48d5a4d80c7aea43fd
hicham <hicha.taha@gmail.com>
2023-03-20 21:43:37 -0700

    copiying contents of the old repo to deploying side

---
+++ README.md
@@ -0,0 +1,8 @@
+# week_6_challenge
+Created an h1 and give it an id for the 5 days weather forecast, with three different cities and made them at the center of the page 
+
+Then moved to create some icones where the data would be shown, for all the 5 days, and inserted an image  inside each icon just for the layout. 
+For js 
+Started with a function named GetInfo and gave it two different variables
+Then I fetch the api and consol.log it 
+With each day of the week.
---
+++ assets/script.js
@@ -0,0 +1,47 @@
+function GetInfo() {
+
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=2b2665f196b4fbaae64edc74fc778eaf')
+.then(response => response.json())
+.then(data => {
+
+    //Getting the min and max values for each day
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
+       
+    }
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+    }
+
+})
+
+
+}
+
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue = "Atlanta";
+    GetInfo();
+}
+
+
+//Getting and displaying the text for the upcoming five days of the week
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//Function to get the correct integer for the index of the days array
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
